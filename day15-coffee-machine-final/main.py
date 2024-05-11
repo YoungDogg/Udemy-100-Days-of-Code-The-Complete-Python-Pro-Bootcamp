1fbc779336c35045ffde1fddfa1ddd456aeb6aa3
Jonghwo Lee <leejonghwo@gmail.com>
2024-05-11 16:17:18 +0900

    add: coffee-machine till TODO:6

---
+++ day15-coffee-machine-final/main.py
@@ -0,0 +1,69 @@
+from controller import clear, check_resources, insert_coin
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO: 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+is_machine_working = True
+money = 0
+while is_machine_working:
+    user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    # TODO: 2. Turn off the Coffee Machine by entering “off” to the prompt.
+    if user_input == "off":
+        clear()
+        is_machine_working = False
+
+    # TODO: 3. Print report.
+    if user_input == "report":
+        print(f'''
+        current resource values
+        Water: {resources["water"]}
+        Milk: {resources["milk"]}
+        Coffee: {resources["coffee"]}
+        Money: {money}
+''')
+
+    # TODO: 4. Check resources sufficient?
+    is_resourceful = check_resources(resources, user_input)
+
+    # TODO: 5. Process coins.
+    if is_resourceful:
+        coin_sum()
+
+    # TODO: 6. Check transaction successful?
+    # money not enough
+    # enough money
+    # too much money
+
+# TODO: 7. Make Coffee.
+print("Here is your {latte}. Enjoy!")
---
+++ higher-lower-start/main.py
@@ -0,0 +1,5 @@
+# main.py
+from control import game_update
+
+if __name__ == '__main__':
+    game_update()
