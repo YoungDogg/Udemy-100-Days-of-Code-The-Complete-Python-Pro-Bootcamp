795881261f2be34f4aa5cfa21f6a0dda4257fb25
YoungDogg <leejonghwo@gmail.com>
2024-05-31 16:38:09 +0900

    add & update: Day18

--- day-18/main.py
+++ day-18/main.py
@@ -1,7 +1,20 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
-timmy_the_turtle.screen()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+
+# for _ in range(4):
+#     timmy_the_turtle.forward(200)
+#     timmy_the_turtle.right(90)
+
+# for _ in range(25):
+#     timmy_the_turtle.pendown()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+
+
 
 screen = Screen()
 screen.exitonclick()
