f16076b81fcb7529b2f7fad9751acb6063b74c5a
YoungDogg <leejonghwo@gmail.com>
2024-06-19 23:02:28 +0900

    update: day-19

--- day_19/turtle_race/day_19_turtle_race.py
+++ day_19/turtle_race/day_19_turtle_race.py
@@ -1,9 +1,30 @@
 from turtle import Turtle
+from typing import List
 
 class TurtleRaceUnit(Turtle):
-    def __init__(self,color):
+    def __init__(self,color:str= "black"):
         super().__init__() # what is the usage of super?
         self.shape("turtle")
         self.color(color)
 
-# TODO: Make a class for turtle group
+class TurtleGroup(TurtleRaceUnit):
+    turtle_objects: List[TurtleRaceUnit] = []
+    turtle_color: List = ["red", "blue", "green", "yellow", "orange", "purple"]
+    y_pos = -300
+    for color in turtle_color:
+        turtle= TurtleRaceUnit()
+        turtle.color(color)
+        turtle.teleport(x=-400,y=y_pos)
+        turtle_objects.append(turtle)
+        y_pos += 120
+
+    def __init__(self):
+        # self.turtle_objects = []
+        # for color in self.turtle_color:
+        #     self.turtle_objects.append(TurtleRaceUnit(color))
+        pass
+    def print(self):
+        print(self.turtle_objects)
+
+
+
--- day_19/turtle_race/main.py
+++ day_19/turtle_race/main.py
@@ -1,12 +1,12 @@
 from turtle import Screen
-from day_19.turtle_race.day_19_turtle_race import  TurtleRaceUnit
+from day_19.turtle_race.day_19_turtle_race import TurtleGroup
 
 def main():
     scn = Screen()
     scn.screensize(canvwidth=500, canvheight=400)
 
-    turt = TurtleRaceUnit(color="red")
-
+    turtle_group = TurtleGroup()
+    turtle_group.print()
     scn.mainloop()
 
 
