b2d12bb4b9ff16234cf8b344d124900b90a4d7e5
YoungDogg <leejonghwo@gmail.com>
2024-08-28 00:56:18 +0900

    update: day-25 question: scope of variable within `with`

--- day_25/main.py
+++ day_25/main.py
@@ -1,11 +1,18 @@
+import csv
 
 def main():
-    with open('weather_data.csv', 'r') as weather_file:
-        data = []
-        for val in weather_file.readlines():
-            data.append(val.strip())
-    print(data)
+    # with open('weather_data.csv', 'r') as weather_file:
+    #     data = []
+    #     for val in weather_file.readlines():
+    #         data.append(val.strip())
+    # print(data)
 
+    with open('weather_data.csv', 'r')  as data_file:
+        data = csv.reader(data_file) # data = list(csv.reader(data_file)) difference?
+        print(data)
+
+        for row in data:
+            print(row)
 
 if __name__ == "__main__":
     main()
