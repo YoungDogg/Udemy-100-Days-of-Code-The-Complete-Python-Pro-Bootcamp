7372deec4eaeef6c2a96fd489a86b42a245f7f52
YoungDogg <leejonghwo@gmail.com>
2024-08-30 17:09:38 +0900

    day 25 update: pandas & squirrel_fur.py

--- day_25/main.py
+++ day_25/main.py
@@ -1,18 +1,64 @@
-import csv
+# import csv
+#
+# def main():
+#     # with open('weather_data.csv', 'r') as weather_file:
+#     #     data = []
+#     #     for val in weather_file.readlines():
+#     #         data.append(val.strip())
+#     # print(data)
+#
+#     with open('weather_data.csv', 'r')  as data_file:
+#         data = list(csv.reader(data_file)) # data = list(csv.reader(data_file)) difference?
+#         temperatures = []
+#
+#         for row in data:
+#             if not row[1] == 'temp':
+#                 temperatures.append(int(row[1]))
+#         print(temperatures)
+#
+
+import pandas
+
 
 def main():
-    # with open('weather_data.csv', 'r') as weather_file:
-    #     data = []
-    #     for val in weather_file.readlines():
-    #         data.append(val.strip())
-    # print(data)
+    data = pandas.read_csv("weather_data.csv")
 
-    with open('weather_data.csv', 'r')  as data_file:
-        data = csv.reader(data_file) # data = list(csv.reader(data_file)) difference?
-        print(data)
+    # data_dict = data.to_dict()
+    # print(data_dict)
 
-        for row in data:
-            print(row)
+    # temp_list = data["temp"].to_list()
+    # print(temp_list)
+
+    avg_temp = 0
+    sum_temp = 0
+    # for temp in temp_list:
+    #     sum_temp += temp
+    # avg_temp = sum_temp / len(temp_list)
+    # avg_temp = sum(temp_list) / len(temp_list)
+    # avg_temp = data["temp"].mean()
+    # print(avg_temp)
+
+    # max_temp = data["temp"].max()
+    # print(max_temp)
+    # print(data.temp)
+
+    # print(data[data.temp == data.temp.max()])
+
+    # monday = data[data.day == "Monday"]
+    # print(monday)
+    # print(monday.temp[0])
+    # to_farenheit = (monday.temp * 9/5) + 32
+    # print(to_farenheit[0])
+
+    # create a dataframe from scratch
+    data_dict = {
+        "students": ["Amy", "James", "Angela"],
+        "scores": [76, 56, 65]
+    }
+    data = pandas.DataFrame(data_dict)
+    print(data)
+    data.to_csv("new_data.csv")
+
 
 if __name__ == "__main__":
     main()
---
+++ day_25/squirrel_fur.py
@@ -0,0 +1,24 @@
+import pandas
+# get number of how many each color squirrels are and make it as csv file
+
+
+def squirrel():
+    data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240830.csv")
+
+    color_diversity_list = list(data["Primary Fur Color"].dropna().unique())
+
+    number_list = []
+    for color in color_diversity_list:
+        number_list.append(len(data[data["Primary Fur Color"] == color]))
+
+    data_dict = {
+        "color":color_diversity_list,
+        "number":number_list
+    }
+
+    data_csv = pandas.DataFrame(data_dict).to_csv("squirrel_fur_population")
+
+
+
+if __name__ == '__main__':
+    squirrel()
