0fac74a0d26a38019700c68749c35f6c1ea8051a
YoungDogg <leejonghwo@gmail.com>
2024-09-11 15:59:02 +0900

    day 25 update: us states guessing

--- day_25/day-25-us-states-game-start/main.py
+++ day_25/day-25-us-states-game-start/main.py
@@ -1,5 +1,6 @@
 import turtle
-
+import pandas
+import tkinter
 
 def main():
     # Make UI: the screen, pop-up window
@@ -9,8 +10,34 @@ def main():
     screen.addshape(image)
 
     turtle.shape(image)
+    # [v] Check if the guess is among the 50 states
+    # [v] get the list the states
+    data = pandas.read_csv("50_states.csv")
+    state_list = list(data["state"])
 
-    screen.exitonclick()
+    # Write correct guesses onto the map
+    # [v] get game screen coordinate by clicking
+    def return_coordinates(x,y):
+        print(f"Clicked coordinates: ({x}, {y})")
+
+    screen.onclick(return_coordinates)
+
+    # [v] get coordinate data list
+    # coordinate_x_list = list(data["x"])
+    # coordinate_y_list = list(data["y"])
+    # coordinate_list = []
+    # for idx in range(len(coordinate_x_list)):
+    #     coordinate = [coordinate_x_list[idx], coordinate_y_list[idx]]
+    #     coordinate_list.append(coordinate)
+    coordinate_list = [list(coord) for coord in zip(data["x"], data["y"])]
+    print(coordinate_list)
+
+
+    # input popup
+    input_return = screen.textinput("Guess the State", "Name?").lower()
+    print(input_return)
+
+    screen.mainloop()
 
 
 if __name__ == "__main__":
--- day_25/day-25-us-states-game-start/readme.md
+++ day_25/day-25-us-states-game-start/readme.md
@@ -6,3 +6,18 @@
 - Use a loop to allow the user to keep guessing 
 - Record the correct guesses in a list
 - Keep track of the score
+
+# what will require
+- [v] Check if the guess is among the 50 states
+  - [v] the array of 50 states
+- Write correct guesses onto the map
+  - get state place coordinate considering click margin
+    - [v] get the coordinate of game window
+    - compare with the coordinate data list
+      - [v] get coordinate data list
+      - _(TODO)_ make condition comparing the coordinates of clicking and the data list 
+    - [v] input popup
+      - handle the error (optional)
+  - condition comparing input and the state list
+    
+
