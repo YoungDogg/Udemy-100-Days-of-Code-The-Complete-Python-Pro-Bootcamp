e40453b374898be0e9f6b617fb877b6c93ea5c51
YoungDogg <leejonghwo@gmail.com>
2025-01-21 11:32:50 +0900

    day_32 just received the project goal

--- day_32/main.py
+++ day_32/main.py
@@ -8,8 +8,23 @@ my_email = os.environ.get("MY_EMAIL")
 receiver_email = os.environ.get("RECEIVER_EMAIL")
 password = os.environ.get("PASSWORD")
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs=receiver_email, msg="email test hahaha")
-connection.close()
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs=receiver_email, msg="email test hahaha")
+#     connection.close()
+
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# day = now.day
+# print(day)
+#
+# date_of_birth = dt.datetime(year=1999,month=1,day=3)
+# print(date_of_birth)
+
+"""
+1. get the current day. 
+2. If it matches some specific day, send a quote from a file 
+"""
