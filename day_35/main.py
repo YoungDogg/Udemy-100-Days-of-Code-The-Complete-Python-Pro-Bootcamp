8e071a4b094ec83336b84a69b7b87d1cc0e3762d
YoungDogg <leejonghwo@gmail.com>
2025-02-06 10:08:40 +0900

    day_35 DONE: print rain if rains

--- day_35/main.py
+++ day_35/main.py
@@ -6,22 +6,28 @@ from dotenv import load_dotenv
 class Main:
     load_dotenv(".env")
 
-    MY_LAT = 37.566536
-    MY_LNG = 126.977966
+    MY_LAT = -12.463440
+    MY_LNG = 130.845642
     my_api:str = os.environ.get("MY_API")
-    url= "https://api.openweathermap.org/data/2.5/weather"
+    url= "https://api.openweathermap.org/data/2.5/forecast"
     params = {"lat": MY_LAT,
               "lon": MY_LNG,
-              "appid": my_api
+              "appid": my_api,
+              "cnt": 4
               }
 
     response = requests.get(url,params)
     response.raise_for_status()
     data = response.json()
 
-    print(f"API: {my_api}")
-    print(data)
+    for each_day in data["list"]:
+        weather_code = each_day["weather"][0]["id"]
+        # print(f"weather code: {weather_code}")
+        if weather_code < 700:
+            print("Bring an Umbrella")
+            break
 
+    # print(data["list"])
 
 if __name__ == "__main__":
     Main()
